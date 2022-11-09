78f47a24004e263c2815f5b7b66279284c85273c
Sunny Pun <sunnyppsp@gmail.com>
2022-11-10 00:43:15 +0800

    :ambulance: adding node and yarn to installer as time is needed for nixOS

--- src/installer.sh
+++ src/installer.sh
@@ -2,4 +2,8 @@ fn_installer() {
     check_no_cmd brew
     # from https://docs.brew.sh/Installation
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+    # temporary :'( as I didn't have time to investigate nixOS yet!
+    brew install node
+    brew install yarn
 }
