a214aa2eb95ad5390faba461e1eb27bb8b67cd2d
Sunny Pun <sunnyppsp@gmail.com>
2023-01-29 01:56:22 +0800

    :sparkles: Terminal multiplexer

--- lib/terminal_multiplexer_definition.sh
+++ lib/terminal_multiplexer_definition.sh
@@ -1,9 +1,9 @@
 parser_definition_terminal_multiplexer() {
     setup REST plus:true help:usage abbr:true -- \
         "Usage: ${2##*/} [options...] [arguments...]" ''
-    msg -- 'TODO!!!!!! example' ''
+    msg -- 'Terminal Multiplexer' ''
     msg -- 'Options:'
-    flag FLAG_A -a -- "message a"
-    param PARAM -d --dir -- "accepts --dir value / --dir=value"
+    flag FLAG_FORCE -f --force -- "force proceeding with installation (with installer)"
+    flag FLAG_INIT -i --init -- "init assets (init.vim and other files) and setup (dependencies)"
     disp :usage -h --help
 }
--- src/terminal_multiplexer.sh
+++ src/terminal_multiplexer.sh
@@ -1,3 +1,11 @@
 fn_terminal_multiplexer() {
-    not_implemented
+    if [[ $FLAG_FORCE -ne 1 ]]; then
+        check_no_cmd tmux
+    fi
+    brew install tmux
+    if [[ $FLAG_INIT -eq 1 ]]; then
+        brew install reattach-to-user-namespace
+
+        cp $script_dir/asset/tmux/tmux.conf ~/.tmux.conf
+    fi
 }
