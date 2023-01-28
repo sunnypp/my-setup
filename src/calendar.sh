8dc8c709ed2c31df17f830ffd58ce20bac857608
Sunny Pun <sunnyppsp@gmail.com>
2023-01-29 01:55:18 +0800

    :sparkles: Calendar apps

--- src/calendar.sh
+++ src/calendar.sh
@@ -1,3 +1,4 @@
 fn_calendar() {
-    not_implemented
+    brew install --cask itsycal
+    manual_todo "Open Itsycal, keyboard shortcut, open at login, 14 days, hide icon, E d-MMM, font size, show weeks number"
 }
